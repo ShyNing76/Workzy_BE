bce7a8e1aa4d079008ab9c7a62236869dc83ded9
lehoangtrong <lehoangtrongcsgl@gmail.com>
2024-09-19 11:42:31 +0700

    fix cors

--- src/server.js
+++ src/server.js
@@ -9,7 +9,13 @@ require('dotenv').config();
 // require('./config/connection')
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+    }
+));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
