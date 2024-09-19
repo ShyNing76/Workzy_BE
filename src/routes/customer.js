c62d54e23840960dbc44084d1666f5b6d3834645
lehoangtrong <lehoangtrongcsgl@gmail.com>
2024-09-19 20:05:26 +0700

    add document

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -4,60 +4,24 @@ const router = require('express').Router();
 
 /**
  * @swagger
- * components:
- *   headers:
- *     Authorization:
- *       description: Access token used for user authentication.
- *       required: true
- *       schema:
- *         type: string
- * /profile:
+ * /customer/profile:
  *   get:
- *     summary: Retrieve user information
- *     description: Returns user information based on the access token.
+ *     summary: Retrieve user profile information
+ *     description: Gets the profile information of the currently authenticated user.
  *     tags:
- *     - User
- *     parameters:
- *       - $ref: '#/components/headers/Authorization'
- *     responses:
- *       '200':
- *         description: Success - User information has been returned
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 userId:
- *                   type: string
- *                   description: ID of the user.
- *                 name:
- *                   type: string
- *                   description: Name of the user.
- *                 email:
- *                   type: string
- *                   description: Email address of the user.
- *       '400':
- *         description: Bad Request - Error message for token validation.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *                   description: Error message.
- *       '500':
- *         description: Internal Server Error - An error occurred while processing the request.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *                   description: Error message.
+ *       - Profile
  */
 router.get('/profile', controller.getUser);
+
+/**
+ * @swagger
+ * /customer/profile:
+ *  post:
+ *   summary: Update user profile information
+ *   description: Updates the profile information of the currently authenticated user.
+ *   tags:
+ *   - Profile
+ */
 router.post('/profile', controller.updateUser);
 
 module.exports = router;
