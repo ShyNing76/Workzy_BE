baab45507e335a4915e4dacc5b5b55a9f30eb6c4
Lê Hoàng Trọng <lehoangtrongcsgl@gmail.com>
2024-09-21 23:23:25 +0700

    Update sync database

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,3 +1,5 @@
+import db from '../models';
+
 const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
@@ -10,6 +12,12 @@ async function connection() {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
+        const NODE_ENV = process.env.NODE_ENV || 'development';
+        if (NODE_ENV === 'development') {
+            db.sequelize.sync({alter: true}).then(() => {
+                console.log('Database sync complete.');
+            });
+        }
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
