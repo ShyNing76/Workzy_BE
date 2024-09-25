1c49fd4030766f7d456bb7c701b242adbd676015
Lê Hoàng Trọng <lehoangtrongcsgl@gmail.com>
2024-09-25 11:15:37 +0700

    add update password

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,6 +1,8 @@
 import db from '../models'
 import jwt from 'jsonwebtoken';
 import moment from "moment";
+import * as hashPassword from '../utils/hashPassword';
+
 
 export const getProfile = (user) => new Promise(async (resolve, reject) => {
         try {
@@ -56,3 +58,39 @@ export const updateProfile = (updateFields) => new Promise(async (resolve, rejec
         reject(error)
     }
 });
+
+export const updatePassword = (updateFields) => new Promise(async (resolve, reject) => {
+    try {
+        const user = await db.User.findOne({
+            where: {
+                user_id: updateFields.user_id
+            }
+        });
+
+        if (!user) {
+            resolve({
+                err: 0,
+                message: 'User not found'
+            })
+        }
+
+        const isPasswordCorrect = hashPassword.comparePassword(updateFields.current_password, user.password);
+
+        if (!isPasswordCorrect) {
+            resolve({
+                err: 0,
+                message: 'Current password is incorrect'
+            })
+        }
+
+        user.password = hashPassword.hashPassword(updateFields.new_password);
+        await user.save();
+
+        resolve({
+            err: 1,
+            message: 'Update password successful'
+        })
+    } catch (error) {
+        reject(error)
+    }
+})
