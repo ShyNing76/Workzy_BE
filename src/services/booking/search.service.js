afa42dc5622f91ffd23d1b61c7500bbe26b914fe
David Quang <tranngvietquang04@gmail.com>
2024-10-06 09:42:34 +0700

    update search

--- src/services/booking/search.service.js
+++ src/services/booking/search.service.js
@@ -1,48 +1,69 @@
+import { col, fn } from "sequelize";
 import db from "../../models";
-import { Op } from "sequelize";
 
-export const searchBuildingService = ({location, workspace_type_name}) => new Promise(async (resolve, reject) => {
-    try {
-        let whereClause = {};
-        let includeClause = [];
-
-        if (location && location !== "") {
-            whereClause.location = location;
-        }
-
-        if (workspace_type_name && workspace_type_name !== "") {
-            includeClause = [{
-                model: db.Workspace,
-                required: true,
-                attributes: [],
-                include: [{
-                    model: db.WorkspaceType,
-                    where: { workspace_type_name: workspace_type_name },
-                    attributes: [],
+export const searchBuildingService = ({ location, workspace_type_name }) =>
+    new Promise(async (resolve, reject) => {
+        try {
+            const buildingsWorkspace = await db.Building.findAll({
+                where: location ? { location } : {},
+                include: {
+                    model: db.Workspace,
                     required: true,
-                }]
-            }];
+                    attributes: [],
+                    include: {
+                        model: db.WorkspaceType,
+                        required: true,
+                        attributes: [],
+                    },
+                },
+                attributes: [
+                    "building_id",
+                    "building_name",
+                    "address",
+                    "google_address",
+                    "location",
+                    "description",
+                    "rating",
+                    [
+                        fn(
+                            "ARRAY_AGG",
+                            fn(
+                                "DISTINCT",
+                                col(
+                                    "Workspaces.WorkspaceType.workspace_type_name"
+                                )
+                            )
+                        ),
+                        "workspace_types",
+                    ],
+                ],
+                raw: true,
+                group: [
+                    "Building.building_id",
+                    "Building.building_name",
+                    "Building.address",
+                    "Building.google_address",
+                    "Building.location",
+                    "Building.description",
+                    "Building.rating",
+                ],
+                nest: true,
+            });
+
+            const buildingsWorkspaceWithWorkspaceTypes = workspace_type_name
+                ? buildingsWorkspace.filter((building) =>
+                      building.workspace_types.includes(workspace_type_name)
+                  )
+                : buildingsWorkspace;
+
+            console.log(buildingsWorkspaceWithWorkspaceTypes);
+
+            resolve({
+                err: 0,
+                message: "Buildings found",
+                data: buildingsWorkspaceWithWorkspaceTypes,
+            });
+        } catch (error) {
+            reject(error);
         }
-
-        const buildings = await db.Building.findAndCountAll({
-            where: whereClause,
-            attributes: {
-                exclude: ["status", "createdAt", "updatedAt"]
-            },
-            include: includeClause,
-            distinct: true
-        });
-
-        if (buildings.count === 0) {
-            return reject("No buildings found");
-        }
-        resolve({
-            err: 0,
-            message: "Buildings found",
-            data: buildings
-        });
-    } catch (error) {
-        reject(error);
-    }
-});
-
+    });
