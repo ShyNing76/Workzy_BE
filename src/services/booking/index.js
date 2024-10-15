26baf0f01ff609c13057a8c2f2313e7c67caa8df
David Quang <tranngvietquang04@gmail.com>
2024-10-15 17:35:39 +0700

    refactor: Remove amenities booking functionality
    - Remove createAmenitiesBookingController and related files
    - Remove addAmenities route from booking.js
    - Remove createAmenitiesBookingService and related files
    - Remove amenities_booking from booking service index
    
    This commit removes the functionality for creating amenities bookings. The createAmenitiesBookingController, addAmenities route, createAmenitiesBookingService, and amenities_booking module have all been deleted. This functionality is no longer needed and has been refactored out of the codebase.

--- src/services/booking/index.js
+++ src/services/booking/index.js
@@ -1,6 +1,5 @@
 export * from "./search.service";
 export * from "./create.service";
 export * from "./get.service";
-export * from "./amenities_booking";
 export * from "./cancel.service";
 export * from "./payment.service";
