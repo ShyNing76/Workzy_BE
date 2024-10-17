7b3c5712aee3808bb9ad9d2924338060d6bb42c5
Lê Hoàng Trọng <lehoangtrongcsgl@gmail.com>
2024-10-17 15:50:28 +0700

    refactor: update package.json scripts, remove deprecated config files, and add sequelize configuration

--- src/routes/v1/booking.js
+++ src/routes/v1/booking.js
@@ -679,10 +679,6 @@ router.post(
                                 format: 'uuid',
                                 example: '123e4567-e89b-12d3-a456-426614174000',
                                 description: 'Booking ID'
-                            },
-                            total_damage_price: {
-                                type: 'number',
-                                example: 500000
                             }
                         }
                     }
