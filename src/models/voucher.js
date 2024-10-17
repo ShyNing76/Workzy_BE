8a876b1f6f4c80c516b42ce20a80f661902b08dd
Lê Hoàng Trọng <lehoangtrongcsgl@gmail.com>
2024-10-17 16:43:30 +0700

    refactor: update GitHub Actions workflow for deployment and fix typo in voucher model

--- src/models/voucher.js
+++ src/models/voucher.js
@@ -28,7 +28,7 @@ module.exports = (sequelize, DataTypes) => {
                 type: DataTypes.STRING,
                 allowNull: false,
             },
-            decription: {
+            description: {
                 type: DataTypes.STRING,
                 defaultValue: "",
             },
